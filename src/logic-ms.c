464beac75c3dc451b05151367cb6ce3a2da2278b
A Sickly Silver Moon <batteryhorsez9@gmail.com>
2026-03-12 21:52:42 -0400

    Fix MS logic (clearing dead entities now adjusts sliplist)

--- src/logic-ms.c
+++ src/logic-ms.c
@@ -1916,6 +1916,29 @@ static void Level_create_clones(Level* self) {
   }
 }
 
+static void check_and_clear_actors(Level* self) {
+  // WIDTH * HEIGHT + 1 so that it's ensured it won't fire unless we're well over max possible alive creatures
+  if (self->ms_state.actor_count > MAP_WIDTH * MAP_HEIGHT + 1) {
+    // warn("%d: filled the actor array, removing dead creatures", self->current_tick);
+    size_t i = 0;
+    while (i < self->ms_state.actor_count) {
+      Actor* actor = &self->actors[i];
+      if (actor->hidden) {
+        memmove(&self->actors[i], &self->actors[i + 1], (self->ms_state.actor_count - i - 1) * sizeof(Actor));
+        self->ms_state.actor_count--;
+        for (size_t j = 0; j < self->ms_state.slip_count; ++j) { // Adjust sliplist entries
+          MsSlipper* slipper = &self->ms_state.slip_list[j];
+          if (slipper->actor - self->actors >= i) {
+            slipper->actor -= 1;
+          }
+        }
+      } else {
+        i++;
+      }
+    }
+  }
+}
+
 static bool ms_init_level(Level* self) {
   memset(self->actors, 0, sizeof(Actor) * MAX_CREATURES);
 
@@ -2083,20 +2106,7 @@ static void ms_tick_level(Level* self) {
   Level_create_clones(self);
 
   // putting this at tick end so that it doesn't break iteration
-  // WIDTH * HEIGHT + 1 so that it's ensured it won't fire unless we're well over max possible alive creatures
-  if (self->ms_state.actor_count > MAP_WIDTH * MAP_HEIGHT + 1) {
-    // warn("%d: filled the actor array, removing dead creatures", level->current_tick);
-    size_t i = 0;
-    while (i < self->ms_state.actor_count) {
-      Actor* actor = &self->actors[i];
-      if (actor->hidden) {
-        memmove(&self->actors[i], &self->actors[i + 1], (self->ms_state.actor_count - i - 1) * sizeof(Actor));
-        self->ms_state.actor_count--;
-      } else {
-        i++;
-      }
-    }
-  }
+  check_and_clear_actors(self);
 }
 
 Ruleset const ms_logic = {.id = Ruleset_MS,
